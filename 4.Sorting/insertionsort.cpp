6b53bbc1f02491cb1213ef816e4ad30753919c7b
Devesh sharma <devsharma13314@gmail.com>
2025-10-08 15:50:44 +0530

    sheet will continue

--- 4.Sorting/insertionsort.cpp
+++ 4.Sorting/insertionsort.cpp
@@ -4,14 +4,15 @@ using namespace std ;
 vector<int> insertion_sort(vector<int>&v){
         int n = v.size();
         for(int i =1;i<n;i++){
-            int j = i-1;
-            int key = v[i] ;
-            while(j>=0 && v[j]>key){
-                v[j+1]=v[j];
+            int j = i;
+            // int key = v[i] ;
+            while(j>0 && v[j-1]>v[j]){
+                swap(v[j],v[j-1]);
                 j--;
 
             }
-            v[j+1] =key ;
+          
+          
 
 
          
